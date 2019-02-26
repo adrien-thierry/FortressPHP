067abb4788b14bc4ded405be300cb2584eca59b3
Adrien FR <adrien.thierryfr@gmail.com>
2019-02-26 16:59:06 +0100

    Add licence

--- start/start.inc.php
+++ start/start.inc.php
@@ -1,5 +1,25 @@
 <?php
 
+/*
+
+FortressPHP
+Copyright (C) 2019  Adrien THIERRY
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 if(!defined("FORTRESSPHP")) header('Location: ../');
 $conf_file = dirname( __FILE__ ) . "/" . "conf.start.php";
 if(!file_exists( $conf_file)) die("No valid conf file found");
